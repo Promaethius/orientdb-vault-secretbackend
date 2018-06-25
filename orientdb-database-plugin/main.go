cd8bced03d5054b373dfe51d9c0d1c041f77895e
Promaethius <anon.clipsy.42@gmail.com>
2018-06-25 14:39:04 -0700

    init

---
+++ orientdb-database-plugin/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/hashicorp/vault/helper/pluginutil"
+	"github.com/promaethius/orientdb-vault-secretbackend"
+)
+
+func main() {
+	apiClientMeta := &pluginutil.APIClientMeta{}
+	flags := apiClientMeta.FlagSet()
+	flags.Parse(os.Args[1:])
+
+	err := orientdb.Run(apiClientMeta.GetTLSConfig())
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
