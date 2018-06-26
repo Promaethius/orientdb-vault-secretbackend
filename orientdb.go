5e4e5da18f3d1bbc7e71cdc3fffd2073a4193c7c
Promaethius <anon.clipsy.42@gmail.com>
2018-06-26 14:07:23 -0700

    Update orientdb.go

--- orientdb.go
+++ orientdb.go
@@ -6,20 +6,31 @@ import (
 	"time"
 	"strings"
 	"error"
-	"encoding/json"
-	"database/sql"
 
 	// REF: Run() api.TLSconfig
 	"github.com/hashicorp/vault/api"
 
-	// REF: UsernameConfig
+	// REF: dbplugin.UsernameConfig https://github.com/hashicorp/vault/blob/master/builtin/logical/database/dbplugin/database.proto
 	"github.com/hashicorp/vault/builtin/logical/database/dbplugin"
+	/*
+	message UsernameConfig {
+		string DisplayName = 1;
+		string RoleName = 2;
+	}
+	*/
 
-	// REF: CredsUtil
+	// REF: credsutil.CredentialsProducer https://github.com/hashicorp/vault/blob/master/plugins/helper/database/credsutil/credsutil.go
 	"github.com/hashicorp/vault/plugins/helper/database/credsutil"
+	/*
+	type CredentialsProducer interface {
+		GenerateUsername(usernameConfig dbplugin.UsernameConfig) (string, error)
+		GeneratePassword() (string, error)
+		GenerateExpiration(ttl time.Time) (string, error)
+	}
+	*/
 )
 
-// REF: Called when plugin is started
+// REF: Called when plugin is started by ./orientdb-vault-secretbackend/main.go
 func Run(apiTLSConfig *api.TLSConfig) error {
 	// Create keep-alive pool when plugin starts.
 	tr := &http.Transport{
@@ -28,7 +39,7 @@ func Run(apiTLSConfig *api.TLSConfig) error {
 		DisableCompression: false,
 	}
 		
-	dbType, err := // TODO: init OrientDB struct here, pass tr into structure for connection pool
+	dbType, err := // TODO(1): init OrientDB struct here, pass tr into structure for connection pool
 	if err != nil {
 		return err
 	}
@@ -40,11 +51,10 @@ func Run(apiTLSConfig *api.TLSConfig) error {
 
 // REF: structure passed as plugin interface to vault.
 type OrientDB struct {
-	*orientdbConnectionProducer
 	credsutil.CredentialsProducer
 	
 	// Init a client that uses the previous connection pool
-	orientdbClient := &http.Client{Transport: tr}
+	orientdbClient
 	
 	// REF: Init(ctx context.Context, config map[string]interface{}, verifyConnection bool) (saveConfig map[string]interface{}, err error)
 	// REF: CreateUser(ctx context.Context, statements dbplugin.Statements, usernameConfig dbplugin.UsernameConfig, expiration time.Time) (username string, password string, err error)
@@ -55,7 +65,8 @@ type OrientDB struct {
 }
 
 func (c *OrientDB) Init(ctx context.Context, config map[string]interface{}, verifyConnection bool) (saveConfig map[string]interface{}, err error) {
-	
+	// TODO(1): tr will come from type init called from Run()
+	orientdbClient := &http.Client{Transport: tr}
 }
 
 // Type returns the name of this plugin
@@ -82,6 +93,7 @@ func (c *OrientDB) RotateRootCredentials(ctx context.Context, statements []strin
 }
 
 func (c * OrientDB) Close() error {
-	
+	// No idea if this is useful. Much easier to just close the connection body after a successful transaction.
+	return nil
 }
 
